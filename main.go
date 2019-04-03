28fc1ce9fec81a017e2982873d55f9a824cef294
Anten Skrabec <cebarks@gmail.com>
2019-04-03 10:37:44 -0600

    prefer to use DATABASE_URL Env variable for database connection
    backup to localhost connection

--- main.go
+++ main.go
@@ -2,50 +2,61 @@ package main
 
 import (
 	"fmt"
-	// "github.com/gorilla/mux"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	// "github.com/gorilla/mux"
 )
 
 func main() {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=gorm dbname=adventuretime sslmode=disable password=")
+	db_url := os.Getenv("DATABASE_URL")
+
+	if db_url == "" {
+		db_url = "host=localhost port=5432 user=gorm dbname=adventuretime sslmode=disable password="
+	}
+
+	fmt.Printf("Using database config string: %s\n", db_url)
+
+	db, err := gorm.Open("postgres", db_url)
 	defer db.Close()
+
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	db.AutoMigrate(&User{}, &Item{}, &UserItem{}, &Quest{})
-	fmt.Println("automigration complete")
+	fmt.Println("Automigration Complete.")
 }
 
 type User struct {
 	gorm.Model
-	UserName   string
+	UserName  string
 	CurrentHp int
-	Xp         int
+	Xp        int
 }
 
 type Item struct {
 	gorm.Model
-	Name       string
-	Attack     int
-	Defense    int
+	Name    string
+	Attack  int
+	Defense int
 }
 
 type UserItem struct {
 	gorm.Model
-	User       User
-	UserID     uint
-	Item     Item
-	ItemID     uint
+	User   User
+	UserID uint
+	Item   Item
+	ItemID uint
 }
 
 type Quest struct {
 	gorm.Model
-	Loction1       string 
-	Loction2       string 
-	Loction3       string 
-	Status         int
-	User           User
-	UserID         uint
+	Loction1 string
+	Loction2 string
+	Loction3 string
+	Status   int
+	User     User
+	UserID   uint
 }
-
