daba12eb829938cfbc1ccb1894a42a8a8ae3ec0d
Anten Skrabec <cebarks@gmail.com>
2019-04-04 13:33:47 -0600

    update model definitions

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,25 +1,25 @@
 package db
 
 import (
-		"fmt"
-		"os"
-		"github.com/jinzhu/gorm"
+	"fmt"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 )
 
-var connection *gorm.DB
+var Connection *gorm.DB
 
 func Connect() {
 	var err error
 	db_url := os.Getenv("DATABASE_URL")
 
 	if db_url == "" {
-		db_url = "\n host=localhost\n port=5432\n user=root\n dbname=adventuretime\n sslmode=disable\n password="
+		db_url = "\n host=localhost\n port=5432\n user=postgres\n dbname=adventuretime\n sslmode=disable\n password="
 	}
-	
+
 	// fmt.Printf("Using database config string: %s\n", db_url)
 
-	connection, err = gorm.Open("postgres", db_url)
+	Connection, err = gorm.Open("postgres", db_url)
 
 	if err != nil {
 		fmt.Println(err)
@@ -29,33 +29,35 @@ func Connect() {
 
 func Migrate() {
 	Connect()
-	connection.AutoMigrate(&User{}, &Item{}, &UserItem{}, &Quest{})
+	Connection.AutoMigrate(&User{}, &Item{}, &UserItem{}, &Quest{})
 	Close()
 	defer fmt.Println("Automigration Complete.")
 }
 
 func Close() {
-	connection.Close()
+	Connection.Close()
 	// defer fmt.Println("Connection to db adventuretime closed.")
 }
 
 func NewQuest(newQuest Quest) {
 	Connect()
-	connection.NewRecord(newQuest)
-	connection.Create(&newQuest)
+	Connection.NewRecord(newQuest)
+	Connection.Create(&newQuest)
 }
+
 type User struct {
 	gorm.Model
-	UserName  string
-	CurrentHp int
-	Xp        int
+	UserName  string `json:"username"`
+	CurrentHp int    `json:"current_hp" gorm:"default: 10"`
+	Xp        int    `json:"current_xp"`
+	Items     []Item `gorm:"many2many:user_items" json:"items"`
 }
 
 type Item struct {
 	gorm.Model
-	Name    string
-	Attack  int
-	Defense int
+	Name    string `json:"name"`
+	Attack  int    `json:"attack"`
+	Defense int    `json:"defense"`
 }
 
 type UserItem struct {
@@ -70,7 +72,7 @@ type Quest struct {
 	Location1 string
 	Location2 string
 	Location3 string
-	Status   int 
-	User     User
-	UserID   uint
+	Status    int
+	User      User
+	UserID    uint
 }
